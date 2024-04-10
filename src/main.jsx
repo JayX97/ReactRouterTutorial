3bce16922f3fc5a3e7355dfc1e174ce690aa3039
JayX97 <jason.fernandezx@yahoo.com>
2024-04-10 03:29:49 -0400

    Added error page to app

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,14 @@ import {
   RouterProvider,
 } from "react-router-dom"
 import './index.css'
+import Root from "./routes/root"
+import ErrorPage from "./error-page"
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World!</div>,
+    element: <Root />,
+    errorElement: <ErrorPage />,
   },
 ]);
 

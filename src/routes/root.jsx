f93eb749f152ad9a892a500e0328c043fa4d9127
JayX97 <jason.fernandezx@yahoo.com>
2024-04-11 01:18:48 -0400

    Added spinner to search bar when searching

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -6,6 +6,7 @@ import {
     Form,
     redirect,
     useNavigation,
+    useSubmit,
  } from "react-router-dom";
 import { getContacts, createContact } from "../contacts";
 
@@ -24,6 +25,13 @@ export async function loader({ request }) {
 export default function Root() {
     const { contacts, q } = useLoaderData();
     const navigation = useNavigation();
+    const submit = useSubmit();
+
+    const searching =
+      navigation.location &&
+      new URLSearchParams(navigation.location.search).has(
+        "q"
+      );
 
     useEffect(() => {
       document.getElementById("q").value = q;
@@ -37,16 +45,20 @@ export default function Root() {
             <Form id="search-form" role="search">
               <input
                 id="q"
+                className={searching ? "loading" : ""}
                 aria-label="Search contacts"
                 placeholder="Search"
                 type="search"
                 name="q"
                 defaultValue={q}
+                onChange={(event) => {
+                  submit(event.currentTarget.form);
+                }}
               />
               <div
                 id="search-spinner"
                 aria-hidden
-                hidden={true}
+                hidden={!searching}
               />
               <div
                 className="sr-only"
